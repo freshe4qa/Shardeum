109ffc1a839aac1e332032324c277cc85d198600
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2023-02-06 19:40:10 +0200

    Update shardeum.sh

--- shardeum.sh
+++ shardeum.sh
@@ -44,7 +44,7 @@ sudo apt install docker.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 apt install npm -y
-npm install -g npm@9.4.1 -y
+npm install -g npm@9.4.1
 
 # download binary
 set -e
